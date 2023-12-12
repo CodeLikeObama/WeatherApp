03fce83181f70746291db6cae76ebbcac7e7e681
Michel Lentzler <michel.lentzler@gmail.com>
2023-12-12 20:32:54 +0100

    searchbar cycle done

--- function.js
+++ function.js
@@ -7,6 +7,8 @@ const humiditytxt = document.getElementById("humidityTxt");
 const temp = document.getElementById("tempTxt");
 const conditionstxt = document.getElementById("conditionsTxt");
 const searchbar = document.getElementById("searchBar");
+let conditionsIcon = document.getElementById("weatherIcon");
+let conditionsIconName;
 const API_KEY = "48452d64f2867b7d828631f4f4768764";
 
 
@@ -25,21 +27,11 @@ function search(){
     cityName.style.display = "block";
 }
 
-function searchButtonFunction()
-{
-    //if (isSearchBarVisable === false) {
-     //   searchbutton.onclick = showSearchbar;
-       // isSearchBarVisable = true;
-   // }
-    //else {
-        searchbutton.onclick = search;
-    //}
-}
 
 //searchbutton.onclick = searchButtonFunction;
 
 
-searchbutton.addEventListener("click",function (){
+searchbutton.addEventListener("click",function () {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchbar.value}&appid=41b4147b4b75fbaba5e0e514756b1051&units=metric`)
         .then(response => response.json())
         .then(data => {
@@ -49,16 +41,53 @@ searchbutton.addEventListener("click",function (){
             let humidity = data.main.humidity + "%";
             let windspd = data.wind.speed.toFixed(1) + "km/h";
             let conditions = data.weather[0].description;
+            conditionsIconName = data.weather[0].main;
 
             windspeed.innerHTML = windspd;
             temp.innerHTML = temperature;
-            conditionstxt.innerHTML = conditions;
+            conditionstxt.innerHTML = conditions.charAt(0).toUpperCase() + conditions.slice(1);;
             humiditytxt.innerHTML = humidity;
             cityName.innerHTML = name;
+
+            changeweatherIcon(conditionsIconName);
             }
         )
+
 
         .catch(err => console.log(err))
 
 })
+searchbutton.addEventListener("click", cyclesearchButton)
 
+function cyclesearchButton() {
+    if (isSearchBarVisable === false) {
+        searchbar.style.display = "block";
+        cityName.style.display = "none";
+        isSearchBarVisable = true;
+    }
+    else {
+        searchbar.style.display = "none";
+        cityName.style.display = "block";
+        isSearchBarVisable = false;
+    }
+}
+
+
+function changeweatherIcon (conditionName) {
+switch (conditionName) {
+    case 'Thunderstorm': conditionsIcon.src = "./weatherIcons/rain.png";;
+    break;
+    case 'Clear': conditionsIcon.src = "./weatherIcons/clear.png";
+    break;
+    case 'Rain' : conditionsIcon.src = "./weatherIcons/rain.png";
+    break;
+    case 'Clouds' : conditionsIcon.src = "./weatherIcons/clouds.png";
+    break;
+    case 'Drizzle': conditionsIcon.src = "./weatherIcons/drizzle.png";
+    break;
+    case 'Atmosphere': conditionsIcon.src = "./weatherIcons/mist.png";
+    break;
+    case 'Snow' : conditionsIcon.src = "./weatherIcons/snow.png";
+    break;
+}
+}
